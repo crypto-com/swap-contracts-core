d36ba78e01eb789d83eaceeb6df84accf4503ffa
Franco NG <franco.ng@crypto.com>
2020-09-03 23:04:56 +0800

    Issue SWAP-2: Add Ropsten to CI

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,15 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const infuraProvider = (network) => {
+  return new HDWalletProvider(
+   process.env.MNEMONIC,
+   `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`
+  )
+}
+
 module.exports = {
   // Uncommenting the defaults below
   // provides for an easier quick-start with Ganache.
@@ -6,21 +18,27 @@ module.exports = {
   // for more details on how to specify configuration options!
   //
   networks: {
-   development: {
-     host: "127.0.0.1",
-     port: 8545,
-     network_id: "*"
-   },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    ropsten: {
+      provider: infuraProvider("ropsten"),
+      network_id: "3",
+      gasPrice: 5000000000, // 5 gwei
+    }
   },
   //
   compilers: {
     solc: {
-      version: "0.5.16"
+      version: "0.6.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 999999
+        }
+      }
     }
   }
 };
