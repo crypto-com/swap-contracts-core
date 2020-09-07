14ca97384f8f63d496e28b2a1f05276f108bb0db
Franco NG <franco.ng@crypto.com>
2020-09-07 23:02:38 +0800

    Issue SWAP-2: Update replaceFactory script

--- scripts/replaceFactory.js
+++ scripts/replaceFactory.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
-const uniswapFactoryFromTruffle = JSON.parse(fs.readFileSync("./build/contracts/UniswapV2Factory.json", "utf8"));
-const uniswapFactoryFromWaffle = JSON.parse(fs.readFileSync("./build/UniswapV2Factory.json", "utf8"));
+const uniswapFactoryFromTruffle = JSON.parse(fs.readFileSync("./build/contracts/CropSwapFactory.json", "utf8"));
+const uniswapFactoryFromWaffle = JSON.parse(fs.readFileSync("./build/CropSwapFactory.json", "utf8"));
 uniswapFactoryFromTruffle.bytecode = uniswapFactoryFromWaffle.bytecode
-fs.writeFileSync("./build/contracts/UniswapV2Factory.json", JSON.stringify(uniswapFactoryFromTruffle), "utf8");
+fs.writeFileSync("./build/contracts/CropSwapFactory.json", JSON.stringify(uniswapFactoryFromTruffle), "utf8");
 
 console.log("[ReplaceFactory] UniswapFactory Truffle now using bytecode compiled from Waffle!")
