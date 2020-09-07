b67c5d5d35cc93141aaf91c7f0faff36511d233b
Franco NG <franco.ng@crypto.com>
2020-09-07 18:33:42 +0800

    Issue SWAP-2: Change waffle version

--- scripts/replaceFactory.js
+++ scripts/replaceFactory.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const uniswapFactoryFromTruffle = JSON.parse(fs.readFileSync("./build/contracts/UniswapV2Factory.json", "utf8"));
-const uniswapFactoryFromWaffle = JSON.parse(fs.readFileSync("./build/waffle/UniswapV2Factory.json", "utf8"));
+const uniswapFactoryFromWaffle = JSON.parse(fs.readFileSync("./build/UniswapV2Factory.json", "utf8"));
 uniswapFactoryFromTruffle.bytecode = uniswapFactoryFromWaffle.bytecode
 fs.writeFileSync("./build/contracts/UniswapV2Factory.json", JSON.stringify(uniswapFactoryFromTruffle), "utf8");
 
