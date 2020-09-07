f4e78afaf0f7295d7728e847bf7c35abe89064d4
ceruleanhu <hulandan123@gmail.com>
2020-09-07 23:47:55 +0800

    Issue SWAP-29: change gas test

--- test/CropSwapFactory.spec.ts
+++ test/CropSwapFactory.spec.ts
@@ -68,7 +68,7 @@ describe('CroDefiSwapFactory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2585002)
+    expect(receipt.gasUsed).to.eq(2590616)
   })
 
   it('setFeeTo', async () => {
