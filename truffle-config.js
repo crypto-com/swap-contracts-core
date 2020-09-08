93ae8dd1c6be24c1a8b7fbc94744634205d44377
Franco NG <franco.ng@crypto.com>
2020-09-08 13:40:16 +0800

    Issue SWAP-32: Add Kovan (and Rinkeby)

--- truffle-config.js
+++ truffle-config.js
@@ -28,6 +28,18 @@ module.exports = {
       network_id: "3",
       gas: 6000000,
       gasPrice: 5000000000, // 5 gwei
+    },
+    rinkeby: {
+      provider: infuraProvider("rinkeby"),
+      network_id: "4",
+      gas: 6000000,
+      gasPrice: 1000000000, // 1 gwei
+    },
+    kovan: {
+      provider: infuraProvider("kovan"),
+      network_id: "42",
+      gas: 6000000,
+      gasPrice: 1000000000, // 1 gwei
     }
   },
   //
